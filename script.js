919fd4301a57b028300fc4583915c19124f317a1
DavidVillarM <144356845+DavidVillarM@users.noreply.github.com>
2023-09-11 12:34:11 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log("Hola Mundo!");
 
 let form = document.getElementById('calculadora');
 form.style.background = 'white';
@@ -11,6 +10,26 @@ const MAN = document.getElementById('man');
 CALCULAR.addEventListener('click', () => {
     const DATO = document.getElementById('peso').value;
     //validar la carga de datos
+    if(DATO > 30){
+        ERROR.style.display = 'none';
+
+        let flujo1 = calcFlujo(DATO) * 1500;
+        let opc1 = flujo1 /24;
+        opc1 = Math.round(opc1);
+
+        let flujo2 = calcFlujo(DATO);
+        let opc2 = flujo2 * 2000;
+        opc2 = opc2 / 24;
+        opc2 = Math.round(opc2);
+
+        let mantenimiento1 = opc1 * 1.5;
+        let mantenimiento2 = opc2 * 1.5;
+        FLU.innerHTML = 'Puede utilizar: Opcion 1: ' + opc1 + ' cc/hr o ' + 'Puede utilizar: Opcion 2: ' + opc2 + ' cc/hr' ;
+        MAN.innerHTML = 'Opcion 1: M+M/2 = ' + mantenimiento1 + ' cc/hr' + ' Opcion 2: M+M/2 = ' + mantenimiento2 + ' cc/hr';
+        FLU.style.display = 'block';
+        MAN.style.display = 'block';
+    } 
+    
     if (DATO > 0){
         ERROR.style.display = 'none';
         let flujo = calcFlujo(DATO);
@@ -19,7 +38,10 @@ CALCULAR.addEventListener('click', () => {
         MAN.innerHTML = 'M+M/2 = ' + mantenimiento + ' cc/hr';
         FLU.style.display = 'block';
         MAN.style.display = 'block';
-    } else {
+
+    } 
+
+    else {
         ERROR.style.display = 'block';
         FLU.style.display = 'none'
         MAN.style.display = 'none'
@@ -29,17 +51,27 @@ CALCULAR.addEventListener('click', () => {
 function calcFlujo(peso){
     let resto = peso;
     let flujo = 0;
-    if (resto>20) {
+    let cc = 0;
+    if (resto > 30){
+        //Superficie corporal = ( (peso * 4) + 7) / (peso + 90)
+        let superCorpo = resto * 4;
+        let aux = resto + 90;
+        superCorpo = superCorpo + 7;
+        superCorpo = superCorpo / aux;        
+        return superCorpo;
+    } else  if (resto>20) {
         let aux = resto-20;
-        flujo += aux*1;
+        cc = aux * 20;
         resto -= aux;
     }
     if (resto>10){
         let aux = resto-10;
-        flujo += aux*2;
+        cc += aux * 50
         resto -= aux;
     }
-    flujo += resto*4;
+    resto = resto * 100;
+    cc += resto;
+    flujo = Math.round(cc / 24);
     return flujo;
 }
 
