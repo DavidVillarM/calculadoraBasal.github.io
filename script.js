a780a305ed4e81228657ab52780c3698121852c6
DavidVillarM <144356845+DavidVillarM@users.noreply.github.com>
2023-09-11 17:30:43 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const MAN = document.getElementById('man');
 CALCULAR.addEventListener('click', () => {
     const DATO = document.getElementById('peso').value;
     //validar la carga de datos
-    if(DATO > 30){
+    if(DATO >= 31){
         ERROR.style.display = 'none';
 
         let flujo1 = calcFlujo(DATO) * 1500;
@@ -24,13 +24,14 @@ CALCULAR.addEventListener('click', () => {
 
         let mantenimiento1 = opc1 * 1.5;
         let mantenimiento2 = opc2 * 1.5;
-        FLU.innerHTML = 'Puede utilizar: Opcion 1: ' + opc1 + ' cc/hr o ' + 'Puede utilizar: Opcion 2: ' + opc2 + ' cc/hr' ;
-        MAN.innerHTML = 'Opcion 1: M+M/2 = ' + mantenimiento1 + ' cc/hr' + ' Opcion 2: M+M/2 = ' + mantenimiento2 + ' cc/hr';
+        FLU.innerHTML = "Puede utilizar: Opcion 1: " + opc1 + " cc/hr o <br>" + "Puede utilizar: Opcion 2: " + opc2 + " cc/hr" ;
+        MAN.innerHTML = "Opcion 1: M+M/2 = " + mantenimiento1 + " cc/hr" + "<br> Opcion 2: M+M/2 = " + mantenimiento2 + " cc/hr";
         FLU.style.display = 'block';
         MAN.style.display = 'block';
+        //console.log("funciona");
     } 
     
-    if (DATO > 0){
+    else if (DATO >=0 && DATO <= 30){
         ERROR.style.display = 'none';
         let flujo = calcFlujo(DATO);
         let mantenimiento = flujo*1.5;
@@ -38,26 +39,28 @@ CALCULAR.addEventListener('click', () => {
         MAN.innerHTML = 'M+M/2 = ' + mantenimiento + ' cc/hr';
         FLU.style.display = 'block';
         MAN.style.display = 'block';
-
+        //console.log("funciona2");
     } 
 
     else {
         ERROR.style.display = 'block';
-        FLU.style.display = 'none'
-        MAN.style.display = 'none'
+        FLU.style.display = 'none';
+        MAN.style.display = 'none';
+        //console.log("funciona3");
     }
 });
 
 function calcFlujo(peso){
-    let resto = peso;
+    let resto = Number(peso);
     let flujo = 0;
     let cc = 0;
     if (resto > 30){
         //Superficie corporal = ( (peso * 4) + 7) / (peso + 90)
-        let superCorpo = resto * 4;
-        let aux = resto + 90;
+        let superCorpo = ((resto * 4) + 7) / (resto + 90);
+        /*let aux = resto + 90;
+        console.log("aux",aux);
         superCorpo = superCorpo + 7;
-        superCorpo = superCorpo / aux;        
+        superCorpo = superCorpo / aux;  */      
         return superCorpo;
     } else  if (resto>20) {
         let aux = resto-20;
